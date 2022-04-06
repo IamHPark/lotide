c07eae06ece2e335f038ad2f38b5dd53b597c8a9
IamHPark <heesoo.p1023@gmail.com>
2022-04-06 01:48:33 +0000

    check exact same arrays

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (arrOne, arrTwo) {
+  let checked = true;
+  for (let i = 0; i < arrOne.length; i++) {
+    if(arrOne[i] === arrTwo[i]){
+      checked;
+    } else {
+      checked = false;
+      console.log(checked);
+      return checked;
+    }
+  }
+  console.log(checked);
+  return checked;
+}
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+// assertEqual( eqArrays([1, 2, 3], [1, 2, 3]) , true );
