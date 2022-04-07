e7369e421e47e68ad23cd3f59c8abbaf6d62989f
IamHPark <heesoo.p1023@gmail.com>
2022-04-07 00:39:23 +0000

    change eqArrays function

--- middle.js
+++ middle.js
@@ -1,15 +1,20 @@
 const eqArrays = function (arrOne, arrTwo) {
   let checked = true;
-  for (let i = 0; i < arrOne.length; i++) {
-    if(arrOne[i] === arrTwo[i]){
-      checked;
-    } else {
-      checked = false;
-      return checked;
+// check if array.length is same 
+  if(arrOne.length === arrTwo.length) {
+    for (let i = 0; i < arrOne.length; i++) {
+      if(arrOne[i] === arrTwo[i]){
+        checked;
+      } else {
+        checked = false;
+        return checked;
+      }
     }
+  } else {
+    return false;
   }
   return checked;
-};
+}
 
 const assertArraysEqual = function(arrOne, arrTwo){
   if( eqArrays(arrOne,arrTwo) ) {
