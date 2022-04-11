465b23a892511a17e5b15340701b43d17468f2ed
IamHPark <heesoo.p1023@gmail.com>
2022-04-11 16:36:38 +0000

    add test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const eqArrays = function (arrOne, arrTwo) {
   let checked = true;
@@ -24,9 +17,4 @@ const eqArrays = function (arrOne, arrTwo) {
   return checked;
 }
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2"])); // => false
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-assertEqual( eqArrays([1, 2, 3], [1, 2, 3]) , true );
+module.exports = eqArrays;
