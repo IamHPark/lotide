546d9cc3ad5af999b87dceef06bb1e7b1c0e101e
IamHPark <heesoo.p1023@gmail.com>
2022-04-11 20:25:40 +0000

    update assertion by using Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,4 @@
-const assertArraysEqual = require('../assertArraysEqual');
+/*const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
 assertArraysEqual( middle([1.2]) , []); // => []
@@ -9,4 +9,41 @@ assertArraysEqual(middle([1, 2, 3, 4, 5]) , [3]); // => [3]
 
 assertArraysEqual(middle([1, 2, 3, 4]) , [2, 3]); // => [2, 3]
 assertArraysEqual(middle([1, 2, 3, 4, 5, 6]) , [3, 4]); // => [3, 4]
+*/
 
+const assert = require('chai').assert;
+const middle = require('../middle');
+const eqArrays = require('../eqArrays');
+
+describe('#middle', () => {
+  it('returns [] for [1,2]', () => {
+    const input = [1,2];  
+    const output = [];
+    const checkEqArr = eqArrays(middle(input),output);
+    assert.strictEqual(checkEqArr,true);
+  });
+  it('returns [2] for [1,2,3]', () => {
+    const input = [1,2,3];  
+    const output = [2];
+    const checkEqArr = eqArrays(middle(input),output);
+    assert.strictEqual(checkEqArr,true);
+  });
+  it('returns [2,3] for [1,2,3,4]', () => {
+    const input = [1,2,3,4];  
+    const output = [2,3];
+    const checkEqArr = eqArrays(middle(input),output);
+    assert.strictEqual(checkEqArr,true);
+  });
+  it('returns [] for [1]', () => {
+    const input = [1];  
+    const output = [];
+    const checkEqArr = eqArrays(middle(input),output);
+    assert.strictEqual(checkEqArr,true);
+  });
+  it('returns ["hi"] for ["say","hi","hello"]', () => {
+    const input = ["say","hi","hello"];  
+    const output = ["hi"];
+    const checkEqArr = eqArrays(middle(input),output);
+    assert.strictEqual(checkEqArr,true);
+  });
+})
