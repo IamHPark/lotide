449f7673869f887d5b005ff2fab4a926a1f04ca7
IamHPark <heesoo.p1023@gmail.com>
2022-04-11 20:19:00 +0000

    update assertion by using Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,14 +1,32 @@
-const assertEqual = require('../assertEqual');
+
+const assert = require('chai').assert;
+const eqArrays = require('../eqArrays');
 const tail = require('../tail');
 
+describe('#tail', () => {
+  it('returns [2,3] for [1,2,3]', () => {
+    const input = [1,2,3];  
+    const output = [2,3];
+    const checkEqArr = eqArrays(tail(input),output);
+    assert.strictEqual(checkEqArr,true);
+  });
+  it('returns ["Lighthouse", "Labs"] for ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    const input = ["Yo Yo", "Lighthouse", "Labs"];  
+    const output = ["Lighthouse", "Labs"];
+    const checkEqArr = eqArrays(tail(input),output);
+    assert.strictEqual(checkEqArr,true);
+  });
+  it('returns [] for [1]', () => {
+    const input = [1];  
+    const output = [];
+    const checkEqArr = eqArrays(tail(input),output);
+    assert.strictEqual(checkEqArr,true);
+  });
+  it('returns [] for []', () => {
+    const input = [];  
+    const output = [];
+    const checkEqArr = eqArrays(tail(input),output);
+    assert.strictEqual(checkEqArr,true);
+  });
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-assertEqual(tail(words).length, 3);
-
-// const words = ["Yo"];
-// console.log(tail(words));
-
-// const words = [];
-// console.log(tail(words));
+})
